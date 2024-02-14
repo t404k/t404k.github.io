1a7399f92061d6473fb32bb9c94ec5ffeafea3a2
t404k <159974169+t404k@users.noreply.github.com>
2024-02-14 07:00:44 +0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,50 @@
+function toggleAudio() {
+  var audioElement = document.getElementById('player')
+  var soundOn = document.getElementById('play')
+  var soundOff = document.getElementById('pause')
+  if (audioElement.paused) {
+    audioElement.play();
+    $(soundOn).show();
+    $(soundOff).hide();
+  } else {
+    audioElement.pause();
+    $(soundOn).hide();
+    $(soundOff).show();
+  }
+}
+
+function blackHeart() {
+  const heart = document.createElement('div');
+  heart.classList.add('heart');
+
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+
+  heart.innerText = '🖤';
+
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+              heart.remove();
+              }, 5000);
+}
+
+setInterval(blackHeart, 500);
+
+function blueHeart() {
+  const heart = document.createElement('div');
+  heart.classList.add('heart');
+
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+
+  heart.innerText = '💙';
+
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+              heart.remove();
+              }, 5000);
+}
+
+setInterval(blueHeart, 500);
